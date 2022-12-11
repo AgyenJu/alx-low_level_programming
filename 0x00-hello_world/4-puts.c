5ad3d121687ecc861e50e21c4d57abd1c33b2fbf
AgyenJu <Justiceagyen@gmail.com>
2022-12-11 07:44:47 -0800

    4-Hello_puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/*
+/**
  * main - main block
  * Return: 0
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzel");	     return(0);;
+	puts("\"programming is like building a multilingual puzzel");
+
+	return (0);
 }
