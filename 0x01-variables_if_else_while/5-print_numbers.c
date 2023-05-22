f88d86eac431ba4571bd089b03de24145ccf4cbe
AgyenJu <Justiceagyen@gmail.com>
2023-05-22 06:01:29 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int sgDig = 0;
+
+	for (; sgDig <= 0; sgDigit++)
+	{
+		printf("%d", sgDigit);
+	}
+	putchar('\n');
+
+	return (0);
+}
